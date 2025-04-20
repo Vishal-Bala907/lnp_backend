61b1f25605a1b1aeea8f11bf899d8626fc0f528b
vishal bala linux <vishal.bala.100@gmail.com>
2025-04-20 21:43:24 +0530

    fix 2

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(
   cors({
     origin: "https://lnp-landing-page.vercel.app", // exact origin of your frontend
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
